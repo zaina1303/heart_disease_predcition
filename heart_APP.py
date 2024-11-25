75e18ffacf5136cfcb5e25bf50724c47375b2909
zaina1303 <zainafakhre1303@gmail.com>
2024-11-25 11:25:14 +0530

    Update heart_APP.py

--- heart_APP.py
+++ heart_APP.py
@@ -6,7 +6,7 @@ import pickle
 # Load the trained RandomForest model and scaler
 with open('rheart_model.pkl', 'rb') as file:
     model = pickle.load(file)
-with open('rhd.pkl', 'rb') as scaler_file:
+with open('rsc.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
 # Define the Streamlit app
